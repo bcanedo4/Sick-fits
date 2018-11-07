143eb7481f0f026c58ef39170dd8cb7b7092fb38
Brian Canedo <bcanedo4@gmail.com>
2018-11-06 16:44:46 -0800

    Added global styling and links for navbar

--- sick-fits/frontend/components/Nav.js
+++ sick-fits/frontend/components/Nav.js
@@ -1,14 +1,21 @@
 import Link from 'next/link';
+import NavStyles from './styles/NavStyles';
 
 const Nav = () => (
-  <div>
-    <Link href="sell">
-      <a>Sell!</a>
+  <NavStyles>
+    <Link href="/items">
+      <a>Items</a>
+    </Link>    
+    <Link href="/signup">
+      <a>Signup</a>
     </Link>
-    <Link href="/">
-      <a>Home</a>
+    <Link href="/orders">
+      <a>Orders</a>
     </Link>
-  </div>
+    <Link href="/me">
+      <a>Account</a>
+    </Link>
+  </NavStyles>
 )
 
 export default Nav;
