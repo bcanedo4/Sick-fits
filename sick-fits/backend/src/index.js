13710cefc12483aaf2266c004b745f3e161d24c8
Brian Canedo <bcanedo4@gmail.com>
2018-11-21 12:42:52 -0800

    Created Permissions page

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -1,8 +1,8 @@
-const cookieParser = require('cookie-parser');
-const jwt = require('jsonwebtoken');
-require('dotenv').config({ path: 'variables.env'});
-const createServer = require('./createServer');
-const db = require('./db');
+const cookieParser = require("cookie-parser");
+const jwt = require("jsonwebtoken");
+require("dotenv").config({ path: "variables.env" });
+const createServer = require("./createServer");
+const db = require("./db");
 
 const server = createServer();
 
@@ -22,13 +22,28 @@ server.express.use((req, res, next) => {
   next();
 });
 
-server.start({
-  cors: {
-    credentials: true,
-    origin: process.env.FRONTEND_URL,
-  }, 
-}, deets => {
-  console.log(`Server is now running on port 
-  http://localhost:${deets.port}`);  
+// 2. Create a middleware that populates the user on each request
+server.express.use(async (req, res, next) => {
+  // if they aren't logged in, skip this
+  if (!req.userId) return next();
+
+  const user = await db.query.user(
+    { where: { id: req.userId } },
+    `{ id, permissions, email, name }`
+  );
+  req.user = user;
+  next();
+});
+
+server.start(
+  {
+    cors: {
+      credentials: true,
+      origin: process.env.FRONTEND_URL
+    }
+  },
+  deets => {
+    console.log(`Server is now running on port 
+  http://localhost:${deets.port}`);
   }
 );
